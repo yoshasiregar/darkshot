58d2e77cccb6a017c3a16f9a4fd27ebd8a5db738
Yoshua Chrismesa Siregar <id.yoshasiregar@gmail.com>
2020-09-12 22:21:29 +0700

    Delete the file at 'check_ocr_langs.py'

--- check_ocr_langs.py
+++
@@ -1,7 +0,0 @@
-from lib.utils import *
-from config import cfg
-
-if check_admin():
-    check_ocr_langs("tesseract", cfg["tessdata_folder"], cfg["ocr_langs"], download=True)
-else:
-    print("Please run it with admin rights.")
